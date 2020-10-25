e4de9dc99aa053dd0b399c2f95aba97940a13947
MaksymKozynets <kozynets.m@gmail.com>
2020-10-25 23:04:54 +0200

    c button

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -1,7 +1,27 @@
-document.querySelectorAll('.calculator' .digits .button)
-   .forEach(button => button. addEventListener('click', digitPressed))
-   function digitPressed (ev){
+document.querySelectorAll('.calculator .digits button')
+   .forEach( button => button.addEventListener('click', digitPressed));
+   function digitPressed(ev) {
        const btn = ev.target;
-       const display = document .querySelector('.display')
-       display .velue += btn.innerText;
+       const display = document.querySelector('.display');
+       display.value += btn.innerText;
    }
+document.querySelectorAll('.calculator .operators button')
+   .forEach( button => button.addEventListener('click', operatorPressed));
+   function operatorPressed(ev) {
+       const btn = ev.target;
+       const display = document.querySelector('.display');
+       display.value += btn.innerText;
+   }
+ 
+document.querySelector('.calculator .allclear button').addEventListener('click', clearPressed);
+    function clearPressed() {
+        const display = document.querySelector('.display');
+        display.value = " ";
+    }
+
+
+document.querySelector('.calculator .eq').addEventListener('click', eqPressed);
+    function eqPressed () {
+        const display = document.querySelector('.display');
+        display.value = eval(display.value);
+    }   
