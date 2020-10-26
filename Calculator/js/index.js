ec2767bbe7e9c60fef58ae10780fc22ed6f53150
MaksymKozynets <kozynets.m@gmail.com>
2020-10-26 12:34:18 +0200

    ce button

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -1,27 +1,28 @@
-document.querySelectorAll('.calculator .digits button')
-   .forEach( button => button.addEventListener('click', digitPressed));
-   function digitPressed(ev) {
-       const btn = ev.target;
-       const display = document.querySelector('.display');
-       display.value += btn.innerText;
-   }
-document.querySelectorAll('.calculator .operators button')
-   .forEach( button => button.addEventListener('click', operatorPressed));
-   function operatorPressed(ev) {
-       const btn = ev.target;
-       const display = document.querySelector('.display');
-       display.value += btn.innerText;
-   }
- 
+document.querySelectorAll('.calculator .digits button').forEach(button=>button.addEventListener('click', digitPressed));
+function digitPressed(ev) {
+    const btn = ev.target;
+    const display = document.querySelector('.display');
+    display.value += btn.innerText;
+}
+document.querySelectorAll('.calculator .operators button').forEach(button=>button.addEventListener('click', operatorPressed));
+function operatorPressed(ev) {
+    const btn = ev.target;
+    const display = document.querySelector('.display');
+    display.value += btn.innerText;
+}
 document.querySelector('.calculator .allclear button').addEventListener('click', clearPressed);
-    function clearPressed() {
-        const display = document.querySelector('.display');
-        display.value = " ";
-    }
-
+function clearPressed() {
+    const display = document.querySelector('.display');
+    display.value = " ";
+}
+document.querySelector('.calculator .clearone button').addEventListener('click', clearonePressed);
+function clearonePressed() {
+    const display = document.querySelector('.display');
+    display.value = display.value.substring(display.value.lenght - 1);
+}
 
 document.querySelector('.calculator .eq').addEventListener('click', eqPressed);
-    function eqPressed () {
-        const display = document.querySelector('.display');
-        display.value = eval(display.value);
-    }   
+function eqPressed() {
+    const display = document.querySelector('.display');
+    display.value = eval(display.value);
+}
